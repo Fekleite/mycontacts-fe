a1f12f8688d94ec691ee816ff3f7214ab8cb77cb
Fernanda Leite <dev.fernandaleite@gmail.com>
2023-11-05 17:36:23 -0300

    refactor: remove input search

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,11 @@
 import logo from '../../assets/images/logo.svg';
 
-import { Container, InputSearchContainer } from './styles';
+import { Container } from './styles';
 
 export default function Header() {
   return (
     <Container>
       <img src={logo} alt="My Contacts" width="201" />
-
-      <InputSearchContainer>
-        <input type="text" placeholder="Pesquisar pelo nome..." />
-      </InputSearchContainer>
     </Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,31 +2,9 @@ import styled from 'styled-components';
 
 export const Container = styled.header`
   margin-top: 74px;
+  margin-bottom: 48px;
 
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
-
-export const InputSearchContainer = styled.div`
-  width: 100%;
-  margin-top: 48px;
-
-  input {
-    width: 100%;
-    height: 50px;
-
-    background-color: #fff;
-
-    border: none;
-    border-radius: 25px;
-    padding: 0 16px;
-
-    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
-    outline: 0;
-
-    &::placeholder {
-      color: ${({ theme }) => theme.colors.gray[200]};
-    }
-  }
 `;
