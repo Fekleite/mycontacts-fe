4548fc47d3c3226a931da6b17c45cc19bc18133f
Fernanda Leite <dev.fernandaleite@gmail.com>
2023-11-14 09:08:45 -0300

    feat: create Select style

---
+++ src/components/Select.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  width: 100%;
+  height: 52px;
+
+  border-radius: 4px;
+  border: 2px solid #fff;
+  outline: none;
+  padding: 0 16px;
+
+  background: #FFF;
+  box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.04);
+
+  font-size: 16px;
+
+  transition: border-color 0.2s ease-in;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray[200]};
+  }
+
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary.main};
+  }
+`;
