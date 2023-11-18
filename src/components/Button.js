420eb0267222f5546e57325568da9bb99a715c40
Fernanda Leite <dev.fernandaleite@gmail.com>
2023-11-18 12:05:14 -0300

    refactor: change button width

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
 export default styled.button`
-  width: 100%;
   height: 52px;
 
   border: none;
   border-radius: 4px;
+  padding: 0 16px;
+
   background-color: ${({ theme }) => theme.colors.primary.main};
   box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.04);
 
