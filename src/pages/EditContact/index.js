1ac203b5b17da4ea46151f88d429f1849001ccf1
Fernanda Leite <dev.fernandaleite@gmail.com>
2023-11-18 11:43:15 -0300

    feat: use ContactForm

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,7 +1,13 @@
+import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
 
 export default function EditContact() {
   return (
-    <PageHeader title="Editar contato" />
+    <>
+      <PageHeader title="Editar contato" />
+
+      <ContactForm buttonLabel="Salvar alterações" />
+
+    </>
   );
 }
