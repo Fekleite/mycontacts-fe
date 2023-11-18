e362b45141d4f9c56375208bfae1e4399e00d404
Fernanda Leite <dev.fernandaleite@gmail.com>
2023-11-18 12:05:33 -0300

    feat: create Modal component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,24 @@
+import Button from '../Button';
+
+import { Container, Overlay, Footer } from './styles';
+
+export default function Modal() {
+  return (
+    <Overlay>
+      <Container>
+        <h1>Titulo</h1>
+        <p>Conteúdo</p>
+
+        <Footer>
+          <button type="button" className="cancel-button">
+            Cancelar
+          </button>
+
+          <Button type="button">
+            Deletar
+          </Button>
+        </Footer>
+      </Container>
+    </Overlay>
+  );
+}
